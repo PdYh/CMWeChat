374c58a6cf57f7138424682c3d5efa3fea6ea9b8
BIGGERMING <wscaiming@gmail.com>
2016-05-09 21:32:52 +0800

    项目初始化 👿

---
+++ CMWeChat/CMWeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMWeChat
+//
+//  Created by 蔡明 on 16/5/9.
+//  Copyright © 2016年 MrCai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
